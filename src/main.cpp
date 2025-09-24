2236519678e20ab9b8684d1777345d79a524d083
Littlewolf_code <Littlewolf_programming@proton.me>
2025-09-24 11:28:37 +0200

    version 1 : obtenir MAC adresse

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,14 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup(){
+    Serial.begin(115200);
+    Serial.print("\nDefault ESP32 MAC Address: ");
+    Serial.println(WiFi.macAddress());
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
